b7113cdeca9df56a400bb250fe8db2c2d444a78d
Jack Fallows <JackFallows@users.noreply.github.com>
2023-07-11 22:28:08 +0100

    Initial commit - includes Google sign in

---
+++ client/webpack.config.js
@@ -0,0 +1,74 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const sveltePreprocess = require('svelte-preprocess');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		index: './src/main.ts'
+	},
+	resolve: {
+		alias: {
+			svelte: path.resolve('node_modules', 'svelte/src/runtime')
+		},
+		extensions: ['.mjs', '.js', '.ts', '.svelte'],
+		mainFields: ['svelte', 'browser', 'module', 'main'],
+		conditionNames: ['svelte', 'browser']
+	},
+	output: {
+		path: path.join(__dirname, '/public/build'),
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].js'
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						compilerOptions: {
+							dev: !prod
+						},
+						emitCss: prod,
+						hotReload: !prod,
+						preprocess: sveltePreprocess({ sourceMap: !prod })
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			}
+		]
+	},
+	mode,
+	watch: !prod,
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+	devtool: prod ? false : 'inline-source-map',
+	devServer: {
+		hot: true,
+		static: {
+			directory: path.join(__dirname, 'public'),
+		}
+	}
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Wishlist web app written in Python/Flask with a Svelte frontend
+
+
+https://realpython.com/flask-google-login/
+
+https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https/page/0
+
