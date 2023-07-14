313c7f4375f9a3d86cc609b800e90b22d7f96297
Jack Fallows <JackFallows@users.noreply.github.com>
2023-07-14 17:19:26 +0100

    a few basic tests

--- client/src/index/App.spec.ts
+++ client/src/index/App.spec.ts
@@ -3,7 +3,17 @@ import App from './App.svelte';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('should work', async ({ mount }) => {
+test('missing username displays "Hello there!"', async ({ mount }) => {
     const component = await mount(App);
     await expect(component).toContainText("Hello there!");
+});
+
+test('provided username displays "Hello {name}!"', async ({ mount }) => {
+    const component = await mount(App, {
+        props: {
+            name: "Jack"
+        }
+    });
+    
+    await expect(component).toContainText("Hello Jack!");
 });
