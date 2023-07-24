cfaec9c2e822040acec84151b1e70990208998de
Jack Fallows <JackFallows@users.noreply.github.com>
2023-07-24 15:35:50 +0100

    make db file set by environment variable

--- data_access/db_connect.py
+++ data_access/db_connect.py
@@ -1,8 +1,11 @@
+import os
 import sqlite3
+
+SQLITE_DB = os.environ.get("SQLITE_DB", "sqlite_db.db")
 
 def get_db_connection():
     db = sqlite3.connect(
-        "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
+        SQLITE_DB, detect_types=sqlite3.PARSE_DECLTYPES
     )
 
     db.row_factory = sqlite3.Row
