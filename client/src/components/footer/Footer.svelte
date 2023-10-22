24ed303c42cf49611276dfacf25c921b3ab47a53
Jack Fallows <JackFallows@users.noreply.github.com>
2023-10-22 21:23:22 +0100

    basic footer

---
+++ client/src/components/footer/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<div class="flex flex-col space-y-1 md:space-y-0 md:flex-row w-full h-24 mt-12 px-4 py-2 bg-slate-100">
+    <div class="grow flex flex-col space-y-1 md:space-y-3">
+        <div class="text-xl grow">
+            Wishlist App
+        </div>
+        <div class="flex space-x-3">
+            <div><a class="hover:text-purple-600" href="https://www.jackfallows.co.uk/" target="_blank">www.jackfallows.co.uk</a> <span class="fa-solid fa-arrow-up-right-from-square"></span></div>
+            <span>|</span>
+            <div><a class="hover:text-purple-600" href="https://github.com/JackFallows/Flask-Svelte-Wishlist-App" target="_blank">GitHub</a> <span class="fa-solid fa-arrow-up-right-from-square"></span></div>
+        </div>
+    </div>
+    <div class="md:self-end">
+        Copyright &copy; {(new Date()).getFullYear()} Jack Fallows. All rights reserved.
+    </div>
+</div>
---
+++ client/src/components/footer/main.ts
@@ -0,0 +1,9 @@
+import NavBar from './Footer.svelte';
+
+const nav_bar = new NavBar({
+	target: document.getElementById("footer-content"),
+	props: {
+	}
+});
+
+export default nav_bar;
