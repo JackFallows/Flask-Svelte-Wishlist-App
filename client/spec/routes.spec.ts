9e7fa033192f2bf95ba5640c6d92b218cff33140
Jack Fallows <JackFallows@users.noreply.github.com>
2023-10-22 19:59:34 +0100

    fix broken test

--- client/spec/routes.spec.ts
+++ client/spec/routes.spec.ts
@@ -5,8 +5,8 @@ const { Views } = makeRoutes("");
 
 describe("Views route", () => {
     it("should be immutable", () => {
-        const original = Views.Wishlist.Edit.to_string();
-        Views.Wishlist.Edit.append(2);
-        expect(Views.Wishlist.Edit.to_string()).toEqual(original);
+        const original = Views.Wishlist.to_string();
+        Views.Wishlist.append(2);
+        expect(Views.Wishlist.to_string()).toEqual(original);
     });
 });
