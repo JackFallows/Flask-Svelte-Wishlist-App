3b921823cec53f4cacbe14c890d8ca61bda11ec5
Jack Fallows <JackFallows@users.noreply.github.com>
2023-10-23 20:15:08 +0100

    show version number in footer

--- client/src/components/footer/Footer.svelte
+++ client/src/components/footer/Footer.svelte
@@ -4,8 +4,9 @@
 
 <div class="flex flex-col space-y-1 md:space-y-0 md:flex-row w-full h-24 mt-12 px-4 py-2 md:py-4 bg-slate-100">
     <div class="grow flex flex-col space-y-1 md:space-y-3">
-        <div class="text-xl grow">
-            Wishlist App
+        <div class="grow flex items-baseline space-x-3">
+            <span class="text-xl">Wishlist App</span>
+            <span class="text-xs text-slate-600">{window.version_number}</span>
         </div>
         <div class="flex space-x-3">
             <div><a class="hover:text-purple-600" href="https://www.jackfallows.co.uk/" target="_blank">www.jackfallows.co.uk</a> <span class="fa-solid fa-arrow-up-right-from-square"></span></div>
