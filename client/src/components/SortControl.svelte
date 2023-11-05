4c9128b0f35d253b74c31d66363da2608cc19ca1
Jack Fallows <JackFallows@users.noreply.github.com>
2023-11-05 21:05:45 +0000

    attempt to improve mobile usability of sort buttons #83

--- client/src/components/SortControl.svelte
+++ client/src/components/SortControl.svelte
@@ -34,7 +34,11 @@
         dispatch(event);
     }
     
-    function touch_start() {
+    function touch_start({ pointerType }: PointerEvent) {
+        if (pointerType !== "touch") {
+            return;
+        }
+        
         const computedStyle = window.getComputedStyle(container);
         if (computedStyle.overflow === "hidden") {
             do_not_move = true;
@@ -42,7 +46,7 @@
     }
 </script>
 
-<div bind:this={container} class="bg-white rounded-full relative min-w-[40px] h-[76px] overflow-hidden hover:overflow-visible" on:touchstart={touch_start}>
+<div bind:this={container} class="bg-white rounded-full relative min-w-[40px] h-[76px] overflow-hidden hover:overflow-visible" on:pointerdown={touch_start}>
     <div class="absolute flex flex-col space-y-1 -top-9 bg-white p-1 right-0 rounded-full hover:outline hover:outline-1 hover:outline-slate-200">
         <IconButton id="{id}-move-top-button" icon="fa-solid fa-angles-up" label="Move to top" small on:click={move_to_top} />
         <IconButton id="{id}-move-up-button" icon="fa-solid fa-angle-up" label="Move up" small on:click={move_up} />
